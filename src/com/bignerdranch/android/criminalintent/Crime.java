b517ff93c4d9382276146d05f0c96fc99c945112
dimitar9 <dimitar9909@gmail.com>
2014-04-28 23:46:21 -0400

    chapter 7 done

--- src/com/bignerdranch/android/criminalintent/Crime.java
+++ src/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,10 +1,13 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 	private UUID mId;
 	private String mTitle;
+	private Date mdate;
+	
 	
 	public Crime() {
 		//Generate unique identifier
--- src/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -16,6 +17,14 @@ public class CrimeActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_crime);
+		
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+		
+		if (fragment == null) {
+			fragment = new CrimeFragment();
+			fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+		}
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
