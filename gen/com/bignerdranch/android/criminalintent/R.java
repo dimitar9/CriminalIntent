18e6d961c0d2d6f244a296f4ef6a119dd5d60e75
dimitar9 <dimitar9909@gmail.com>
2014-05-04 23:13:30 -0400

    finished chapter 12

--- gen/com/bignerdranch/android/criminalintent/R.java
+++ gen/com/bignerdranch/android/criminalintent/R.java
@@ -995,20 +995,21 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050025;
-        public static final int action_settings=0x7f050044;
+        public static final int action_settings=0x7f050045;
         public static final int activity_chooser_view_content=0x7f050026;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002e;
         public static final int collapseActionView=0x7f05000d;
-        public static final int crime_date=0x7f05003f;
-        public static final int crime_list_item_dateTextView=0x7f050043;
-        public static final int crime_list_item_solvedCheckBox=0x7f050041;
-        public static final int crime_list_item_titleTextView=0x7f050042;
-        public static final int crime_solved=0x7f050040;
-        public static final int crime_title=0x7f05003e;
+        public static final int crime_date=0x7f050040;
+        public static final int crime_list_item_dateTextView=0x7f050044;
+        public static final int crime_list_item_solvedCheckBox=0x7f050042;
+        public static final int crime_list_item_titleTextView=0x7f050043;
+        public static final int crime_solved=0x7f050041;
+        public static final int crime_title=0x7f05003f;
         public static final int default_activity_button=0x7f050029;
         public static final int dialog=0x7f05000e;
+        public static final int dialog_date_datePicker=0x7f05003e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050031;
@@ -1104,9 +1105,10 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int activity_fragment=0x7f030017;
-        public static final int fragment_crime=0x7f030018;
-        public static final int list_item_crime=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int dialog_date=0x7f030018;
+        public static final int fragment_crime=0x7f030019;
+        public static final int list_item_crime=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
         public static final int crime=0x7f0c0000;
